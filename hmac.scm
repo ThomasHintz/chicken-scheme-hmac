3b221a88cee30efcac5029be3b2de6fc55ef37cf
t@thintz.com <t@thintz.com>
2011-08-31 07:59:18 -0700

    removed dependency on srfi-4-utils as it was gpl and not compatible with bsd

--- hmac.scm
+++ hmac.scm
@@ -6,16 +6,15 @@
   (hmac)
 
 (import scheme chicken srfi-4 srfi-13)
-(use srfi-4-utils message-digest-port)
+(use message-digest-port)
 
 ; taken from example at http://wiki.call-cc.org/drupal-xml-rpc
 (define (hmac key digest-primitive #!optional (block-size 64))
   (when (> (string-length key) block-size)
     (set! key (call-with-output-digest digest-primitive (cut display key <>) 'string)))
   (set! key (string-pad-right key block-size (integer->char 0)))
-  (set! key (blob->u8vector (string->blob key)))
-  (let ((ipad (blob->string (u8vector->blob (u8vector-map (lambda (v) (bitwise-xor v #x36)) key))))
-        (opad (blob->string (u8vector->blob (u8vector-map (lambda (v) (bitwise-xor v #x5c)) key)))))
+  (let ((ipad (string-map (lambda (c) (integer->char (bitwise-xor (char->integer c) #x36))) key))
+	(opad (string-map (lambda (c) (integer->char (bitwise-xor (char->integer c) #x5c))) key)))
     (lambda (message)
       (call-with-output-digest digest-primitive (cut display (string-append opad (call-with-output-digest digest-primitive (cut display (string-append ipad message) <>) 'string)) <>) 'string))))
 
