ThomasHintz/chicken-scheme-hmac

commit b52b480a81c80338090ee1690f3a10a677c2abae

Cleanup formatting.

hmac.scm
@@ -8,7 +8,6 @@
 (import scheme chicken srfi-4 srfi-13)
 (use message-digest-port)
 
-; taken from example at http://wiki.call-cc.org/drupal-xml-rpc
 (define (hmac key digest-primitive #!optional (block-size 64))
   (when (> (string-length key) block-size)
     (set! key (call-with-output-digest digest-primitive (cut display key <>) 'string)))
@@ -16,6 +15,15 @@
   (let ((ipad (string-map (lambda (c) (integer->char (bitwise-xor (char->integer c) #x36))) key))
 	(opad (string-map (lambda (c) (integer->char (bitwise-xor (char->integer c) #x5c))) key)))
     (lambda (message)
-      (call-with-output-digest digest-primitive (cut display (string-append opad (call-with-output-digest digest-primitive (cut display (string-append ipad message) <>) 'string)) <>) 'string))))
+      (call-with-output-digest
+        digest-primitive
+        (cut display
+             (string-append opad
+                            (call-with-output-digest
+                              digest-primitive
+                              (cut display (string-append ipad message) <>)
+                              string))
+             <>)
+        'string))))
 
 )

commit d759083e9dcd52b52cea8770fb67490809da741a

Fix invalid arg type bug introduced in b52b480.

hmac.scm
@@ -22,7 +22,7 @@
                             (call-with-output-digest
                               digest-primitive
                               (cut display (string-append ipad message) <>)
-                              string))
+                              'string))
              <>)
         'string))))
 